df307dae954e9016e5495e3fd5f1a944cc60b86a
Hassan Amin <intechsolutions.pk@gmail.com>
2020-10-27 15:26:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           <h1>Hassan Amin</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
