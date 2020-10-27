a097b7c49b2d3a3c1ed29c2b679721f566c1fd6f
Hassan Amin <intechsolutions.pk@gmail.com>
2020-10-27 18:42:51 +0500

    update text

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <h1>Hassan Amin</h1>
+          My React App
         </p>
       </header>
     </div>
